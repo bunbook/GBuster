e9415035e170844a9e5e2903a01e622e975d6119
bunbook <25857032+bunbook@users.noreply.github.com>
2018-05-07 11:49:09 +0900

    [add, update] シーン更新したりジェネリックなシングルトン追加したり

---
+++ Assets/GBuster/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : SingletonMonoBehaviour<GameManager>
+{
+
+    #region フィールド
+
+    
+
+	#endregion
+
+
+	#region プロパティ
+	
+    
+
+	#endregion
+
+
+	#region Unity メソッド
+	
+	/// <summary> 
+	/// 初期化処理
+	/// </summary>
+	void Awake ()
+	{
+        InitSingleton();
+	}
+	
+	/// <summary> 
+	/// 更新前処理
+	/// </summary>
+	void Start () 
+	{
+		
+	}
+	
+	/// <summary> 
+	/// 更新処理
+	/// </summary>
+	void Update () 
+	{
+		
+	}
+	
+	#endregion
+
+
+	#region メソッド
+	
+	#endregion
+
+}
---
+++ Assets/GBuster/Scripts/SingletonMonoBehaviour.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// ジェネリックなシングルトンクラス
+/// （参考）http://naichilab.blogspot.jp/2013/11/unitymanager.html
+/// </summary>
+/// <typeparam name="T">Monobehaviourを継承している型</typeparam>
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+
+    #region フィールド
+
+    private static T instance;
+
+	#endregion
+
+
+	#region public プロパティ
+
+    public static T Instance
+    {
+        get
+        {
+            instance = instance ?? (T)FindObjectOfType(typeof(T));
+
+            if (instance == null)
+                Debug.LogError(typeof(T) + "is nothing");
+            
+            return instance;
+        }
+    }
+
+    #endregion
+
+
+    #region protected メソッド
+
+    /// <summary>
+    /// シングルトンの初期設定
+    /// </summary>
+    protected void InitSingleton()
+    {
+        bool isRootObject = !this.transform.parent;
+
+        if (isRootObject)
+        {
+            Debug.Log("rootです");
+            if (this != Instance)
+                Destroy(this.gameObject);
+            else
+                DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Debug.Log("rootではないです");
+            if (this != Instance)
+                Destroy(this);
+        }
+    }
+
+    #endregion
+
+    /// <summary>
+    /// 使用例
+    /// </summary>
+    /*
+    using UnityEngine;
+    public class GameManager : SingletonMonoBehaviour<GameManager>
+    {
+        public void Awake()
+        {
+            InitSingleton();
+        }
+    }
+    */
+
+}
